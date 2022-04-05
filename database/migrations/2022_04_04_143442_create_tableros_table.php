536a52bad9d1b198c2cc2b3c0124c849e22345b4
Fabian Duran <ingfabianavellaneda@gmail.com>
2022-04-05 16:36:38 -0500

    Se cambian las migraciones

--- database/migrations/2022_04_04_143442_create_tableros_table.php
+++ database/migrations/2022_04_04_143442_create_tableros_table.php
@@ -15,15 +15,15 @@ return new class extends Migration
     {
         Schema::create('tableros', function (Blueprint $table) {
             $table->id();
-            $table->char('campo_1', 1)->default('');
-            $table->char('campo_2', 1)->default('');;
-            $table->char('campo_3', 1)->default('');;
-            $table->char('campo_4', 1)->default('');;
-            $table->char('campo_5', 1)->default('');;
-            $table->char('campo_6', 1)->default('');;
-            $table->char('campo_7', 1)->default('');;
-            $table->char('campo_8', 1)->default('');;
-            $table->char('campo_9', 1)->default('');;
+            $table->char('campo_1', 1)->default('-');
+            $table->char('campo_2', 1)->default('-');;
+            $table->char('campo_3', 1)->default('-');;
+            $table->char('campo_4', 1)->default('-');;
+            $table->char('campo_5', 1)->default('-');;
+            $table->char('campo_6', 1)->default('-');;
+            $table->char('campo_7', 1)->default('-');;
+            $table->char('campo_8', 1)->default('-');;
+            $table->char('campo_9', 1)->default('-');;
             $table->foreignId('partida_id');
             $table->foreign('partida_id')->references('id')->on('partidas');
             $table->timestamps();
